163a00663d6b5ce36c86272d30eaeb46a6475c59
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-11 13:31:47 +0530

    feat: structs

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+type Movie struct {
+	id       string    `json: "id"`
+	isbn     string    `json: "isbn"`
+	title    string    `json: "title"`
+	director *Director `json: "director"`
+}
+
+type Director struct {
+	firstName string `json: "firstname"`
+	lastName  string `json: "lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+}
