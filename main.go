9e745f2f932ae363dfacc1dc075c5558ca19dc39
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-11 15:33:24 +0530

    refactor: structs

--- main.go
+++ main.go
@@ -12,15 +12,15 @@ import (
 )
 
 type Movie struct {
-	id       string    `json: "id"`
-	isbn     string    `json: "isbn"`
-	title    string    `json: "title"`
-	director *Director `json: "director"`
+	ID       string    `json:"id"`
+	ISBN     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
-	firstName string `json: "firstname"`
-	lastName  string `json: "lastname"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
 
 var movies []Movie
@@ -34,7 +34,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range movies {
-		if item.id == params["id"] {
+		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
@@ -46,17 +46,19 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range movies {
-		if item.id == params["id"] {
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
+			return
 		}
 	}
+	http.Error(w, "Movie not found", http.StatusNotFound)
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.id = strconv.Itoa(rand.Intn(10000000))
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
@@ -65,26 +67,26 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range movies {
-		if item.id == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
+		if item.ID == params["id"] {
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.id = params["id"]
-			movies = append(movies, movie)
+			movie.ID = params["id"]
+			movies[index] = movie
 			json.NewEncoder(w).Encode(movie)
 			return
 		}
 	}
+	http.Error(w, "Movie not found", http.StatusNotFound)
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies, Movie{id: "001", isbn: "36366363", title: "Interstellar", director: &Director{firstName: "Christopher", lastName: "Nolan"}})
-	movies = append(movies, Movie{id: "002", isbn: "43743778", title: "Inception", director: &Director{firstName: "Christopher", lastName: "Nolan"}})
+	movies = append(movies, Movie{ID: "001", ISBN: "36366363", Title: "Interstellar", Director: &Director{FirstName: "Christopher", LastName: "Nolan"}})
+	movies = append(movies, Movie{ID: "002", ISBN: "43743778", Title: "Inception", Director: &Director{FirstName: "Christopher", LastName: "Nolan"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{ID}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
