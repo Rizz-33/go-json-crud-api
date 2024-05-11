01e1e8f3bb9939140c35506f1fadd4a1eac13375
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-11 14:42:43 +0530

    feat: createMovie func

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -48,6 +50,15 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(item)
 		}
 	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.id = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
